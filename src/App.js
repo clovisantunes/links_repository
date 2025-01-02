8c5a0535c2006510dcdbd24a5a740f1b3486a751
unknown <clovissantannaa@gmail.com>
2025-01-02 15:00:04 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LinksPage from "./components/LinksPage/LinksPage";
+import './globals.scss';
+
+const App = () => {
+  return (
+    <div>
+      <LinksPage />
+    </div>
+  );
+};
+
+export default App;
